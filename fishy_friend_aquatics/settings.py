77d21a1f618bf060c78ff8715c738894ef5c5370
muhammedmuhzin007 <muhzinmuhammed4@gmail.com>
2026-01-01 09:35:41 +0530

    jan-1

--- fishy_friend_aquatics/settings.py
+++ fishy_friend_aquatics/settings.py
@@ -68,8 +68,16 @@ WSGI_APPLICATION = 'fishy_friend_aquatics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'fishy_friend_aquatics'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
